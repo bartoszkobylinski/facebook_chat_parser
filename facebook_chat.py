e9dedbc6f796be701b3cda4ca998efc658a5e822
bartek <bartosz.kobylinski@gmail.com>
2020-08-24 23:19:03 +0200

    fixing encoding problem

--- facebook_chat.py
+++ facebook_chat.py
@@ -1,19 +1,28 @@
+"""
+File with FacebookChat class and all its function
+"""
 import json
-import math
-import typing
 import time
 from collections import Counter
 
-fb_file_list = ['nie_ten_watek_1.json','nie_ten_watek_2.json','nie_ten_watek_3.json','nie_ten_watek_4.json','nie_ten_watek_5.json']
+fb_file_list = ['nie_ten_watek_1.json',
+                'nie_ten_watek_2.json',
+                'nie_ten_watek_3.json',
+                'nie_ten_watek_4.json',
+                'nie_ten_watek_5.json']
 
 
 class FacebookChat:
+    """
+    class containing information get from file containg facebook messanger chat
+    """
     def __init__(self,
                  file: str = None,
                  total_chat_messages_number=0,
                  total_chat_characters_number=0,
                  total_chat_photos_number=0,
                  total_chat_gifs_number=0,
+                 total_reactions_number=0
                  ):
         self._file = file
         self._total_chat_messages_number = total_chat_messages_number
@@ -23,8 +32,11 @@ class FacebookChat:
         self._chat_messages = None
 
     def __str__(self):
-        return f'''Facebook chat from file {self._file} has {self._total_chat_messages_number} messages, {self._total_chat_characters_number} characters, 
-     {self._total_chat_photos_number} photos, {self._total_chat_gifs_number} gifs.'''
+        return f'''Facebook chat from file {self._file} has
+                   {self._total_chat_messages_number} messages,
+                   {self._total_chat_characters_number} characters,
+                   {self._total_chat_photos_number} photos,
+                   {self._total_chat_gifs_number} gifs.'''
 
     @property
     def messages(self):
@@ -43,32 +55,16 @@ class FacebookChat:
         self._file = file
         self._total_chat_gifs_number += self.get_chat_total_gifs_number()
         self._total_chat_messages_number += len(self.messages)
-        self._total_chat_characters_number += self.get_chat_total_characters_number()
+        self._total_chat_characters_number += (
+            self.get_chat_total_characters_number())
         self._total_chat_photos_number += self.get_chat_total_photos_number()
         self._chat_messages = None
-    '''
-    @property
-    def total_chat_messages_number(self):
-        return self._total_chat_messages_number
 
-    @property
-    def total_chat_photos_number(self):
-        return self._total_chat_photos_number
-
-    @property
-    def total_chat_gifs_number(self):
-        return self.__total_chat_gifs_number
-
-    @property
-    def total_chat_characters_number(self):
-        return self.total_chat_characters_number
-    '''
     def _parse_data_from_file(self):
-        # Add file testing
         with open(self.file, encoding='latin-1') as facebook_chat_file:
             facebook_chat_data = json.load(facebook_chat_file)
         return facebook_chat_data
-    
+
     def set_content(self):
         return self._chat_messages
 
@@ -76,8 +72,12 @@ class FacebookChat:
         fb_chat_data = self._parse_data_from_file()
         participants = []
         for participant in fb_chat_data['participants']:
+            participant = self.correct_string_decoding(participant['name'])
             participants.append(participant)
         return participants
+
+    def correct_string_decoding(self, string):
+        return string.encode('iso-8859-1').decode('utf-8)')
 
     def get_chat_total_photos_number(self):
         photos_counter = 0
@@ -96,7 +96,8 @@ class FacebookChat:
     def get_chat_total_characters_number(self):
         total_characters_number = 0
         for message in self.messages:
-            total_characters_number += len(message.get('content', ''))
+            total_characters_number += len(
+                self.correct_string_decoding(message.get('content', '')))
         return total_characters_number
 
     def get_chat_total_words_number(self):
@@ -110,7 +111,9 @@ class FacebookChat:
         messages_string = ''
         word = word.lower()
         for message in self.messages:
-            messages_string = messages_string + message.get('content', ' ').lower() + ' '
+            messages_string = messages_string + (
+                self.correct_string_decoding(message.get(
+                    'content', ' ')).lower()) + ' '
         messages_string = messages_string.split()
         frequency_counter = Counter(messages_string)
         frequency_counter = frequency_counter.get(word, int(0))
@@ -119,40 +122,43 @@ class FacebookChat:
     def determine_chat_word_frequency_by_word_length(self, word_len: int):
         messages = []
         for message in self.messages:
-            message = message.get('content', ' ').lower()
+            message = self.correct_string_decoding(
+                message.get('content', ' ')).lower()
             message = message.split()
             for word in list(message):
                 if len(word) < word_len:
                     message.remove(word)
             messages += message
         frequency_counter = Counter(messages)
-        return frequency_counter.most_common(10)
-
-
-
-
-
-
+        return frequency_counter.most_common(word_len)
 
 # def get_facebook_statistic(list_file)
-'''
+
+
 start = time.time()
 b = FacebookChat()
+
 
 for file in fb_file_list:
     b.file = file
     b.messages
     content = b.set_content()
-    print(f"That is len of content {len(content)}")
-    
-   
+    # print(b)
+    # print(f"That is len of content {len(content)}")
+    participant = b.get_participants_of_chat()
+    # print(f"That is your participant {participant}")
 
-freq_of_word  = b.find_chat_word_frequency('chuj')
-freq = b.determine_chat_word_frequency_by_word_length(4)
+freq_of_word = b.find_chat_word_frequency('')
+# print(freq_of_word)
+freq = b.determine_chat_word_frequency_by_word_length(7)
+# print(freq)
+
+b.get_participants_of_chat()
 
 end = time.time()
 result = end - start
 print(result)
+
 '''
 # print(f"Total messages is: {total_messages}")
 # print(f"Total characters is: {total_characters}")
@@ -162,4 +168,4 @@ print(result)
 # print(f"Total gifs send on chat: {total_gifs_number}")
 #
 # print(a)
-
+'''
--- participant.py
+++ participant.py
@@ -1,26 +1,24 @@
-import time
-from dawi import FacebookChat
+
+from facebook_chat import FacebookChat
 
 
 class Participant(FacebookChat):
-    
+
     def __init__(self, name,
-                total_messages_number=0,
-                total_characters_number=0,
-                total_photos_number=0,
-                total_gifs_number=0):
-        self._name = name 
+                 total_messages_number=0,
+                 total_characters_number=0,
+                 total_photos_number=0,
+                 total_gifs_number=0):
+        self._name = name
         self._total_messages_number = total_messages_number
         self._total_characters_number = total_characters_number
         self._total_photos_number = total_photos_number
         self._total_gifs_number = total_gifs_number
         self._messages = None
 
-
-    def __str__ (self):
+    def __str__(self):
         return f"Your name is {self._name} and messages {len(self._messages)}"
 
-    
     def get_messages(self, messages):
         if self._messages:
             return self._messages
@@ -31,25 +29,27 @@ class Participant(FacebookChat):
                     participant_messages.append(message)
             self._messages = participant_messages
             return self._messages
-    
-
 
 
 chat = FacebookChat()
 
-for file in ['nie_ten_watek_1.json','nie_ten_watek_2.json','nie_ten_watek_3.json','nie_ten_watek_4.json','nie_ten_watek_5.json']:
+for file in ['nie_ten_watek_1.json',
+             'nie_ten_watek_2.json',
+             'nie_ten_watek_3.json',
+             'nie_ten_watek_4.json',
+             'nie_ten_watek_5.json']:
     chat.file = file
     chat.messages
     participants = chat.get_participants_of_chat()
-    #print(chat.messages[4:6])
-    #print(chat)
-    #print(f"that is content {content[56]}")
+    print(participants)
+    # print(chat.messages[4:6])
+    # print(chat)
+    # print(f"that is content {content[56]}")
     for participant in participants:
-        a = Participant(participant.get('name',''))
-        participant_messages = a.get_messages(chat.set_content()) 
+        participant_messages = chat.get_messages(chat.set_content())
+        user = Participant(participant)
+        print(user)
         print(participant_messages[3:4])
-        print(a.get_chat_total_gifs_number())
+        # print(a.get_chat_total_gifs_number())
         print(f"that is len {len(participant_messages)}")
-        #print(a._messages[4:15])
-       
-       
+        # print(a._messages[4:15])
