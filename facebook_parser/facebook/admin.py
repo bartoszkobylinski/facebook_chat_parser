7e0a65ab5693aaa5af81b19171e65e3223d906df
bartek <bartosz.kobylinski@gmail.com>
2020-10-12 19:20:23 +0200

    ADDING MODELS
    - adding model for words which has from 4 to 10 characters
    - adding model to register in admin

--- facebook_parser/facebook/admin.py
+++ facebook_parser/facebook/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
-from .models import FacebookChat, Participant
+from facebook.models import (FacebookChat, Participant, FourCharWord, FiveCharWord,
+                             SixCharWord, SevenCharWord, EighthCharWord, NineCharWord, 
+                             TenAndMoreCharWord)
 
 
 admin.site.register(FacebookChat)
 admin.site.register(Participant)
+admin.site.register(FourCharWord)
+admin.site.register(FiveCharWord)
+admin.site.register(SixCharWord)
+admin.site.register(SevenCharWord)
+admin.site.register(EighthCharWord)
+admin.site.register(NineCharWord)
+admin.site.register(TenAndMoreCharWord)
